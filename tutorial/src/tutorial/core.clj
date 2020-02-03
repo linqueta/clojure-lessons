57fb14ccccdb269c04eb845147684df7f42b5283
linqueta <lincolnrodrs@gmail.com>
2020-02-03 06:49:07 -0300

    know how to declare a var

--- tutorial/src/tutorial/core.clj
+++ tutorial/src/tutorial/core.clj
@@ -1,7 +1,16 @@
 (ns tutorial.core
   (:gen-class))
 
+(def random 10)
+
+(def aDouble 1.233)
+
+(def someBoolean true)
+
+;; comment
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
