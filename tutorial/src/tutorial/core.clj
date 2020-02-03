3a2503485761b4a177c878c6426d43b3e243b782
linqueta <lincolnrodrs@gmail.com>
2020-02-03 07:05:39 -0300

    format strings

--- tutorial/src/tutorial/core.clj
+++ tutorial/src/tutorial/core.clj
@@ -1,19 +1,18 @@
 (ns tutorial.core
   (:gen-class))
 
-(def random 10)
-
-(def aDouble 1.233)
-
-(def someBoolean true)
-
-;; comment
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+
+  (def aString "Hello")
   (def aLong 15)
-  (nil? aLong)
+  (def aDouble 1.234)
+  (format "This is string %s" aString)
+  (format "5 spaces and %5d" aLong)
+  (format "Leading zeros %04d" aLong)
+  (format "%-4d left justified" aLong)
+  (format "3 decimals %.3f" aDouble)
   )
 
 (-main)
