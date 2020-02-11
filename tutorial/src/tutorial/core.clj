5e3db87b7ae0a460cceae85f95db2827254b8cc1
linqueta <lincolnrodrs@gmail.com>
2020-02-11 09:04:08 -0300

    add agent example

--- tutorial/src/tutorial/core.clj
+++ tutorial/src/tutorial/core.clj
@@ -15,10 +15,27 @@
   (swap! atomEx inc)
   (println "Increment x" @atomEx))
 
+(defn agent-ex
+  []
+
+  (def tickets-sold (agent 0))
+  (send tickets-sold + 15)
+  (println)
+  (println "Tickets " @tickets-sold)
+
+  (send tickets-sold + 10)
+  (await-for 100 tickets-sold)
+  (println "Tickets " @tickets-sold)
+
+  (shutdown-agents)
+  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (atom-ex  5))
+  (atom-ex  5)
+  (agent-ex)
+  )
 
 (-main)
